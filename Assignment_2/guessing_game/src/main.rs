c1024dc447878ef59706d3420877615da20684c8
Jacco Brandt <djekkoo@hotmail.com>
2017-12-07 14:23:42 +0100

    added guessing_game tutorual

--- Assignment_2/guessing_game/src/main.rs
+++ Assignment_2/guessing_game/src/main.rs
@@ -1,3 +1,39 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    
+    let guess_me = rand::thread_rng().gen_range(1, 101);
+    // println!("Secret number: {}", guess_me);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&guess_me) {
+            Ordering::Equal => { 
+                println!("You win");
+                break;
+            },
+            Ordering::Less  => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
