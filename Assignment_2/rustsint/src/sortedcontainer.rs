86fb45ca6f09ae2641d3d06035c16ed699a0ca88
Olaf Haalstra <olaf_haalstra@hotmail.com>
2017-12-20 19:04:58 +0100

    Finished erase function

--- Assignment_2/rustsint/src/sortedcontainer.rs
+++ Assignment_2/rustsint/src/sortedcontainer.rs
@@ -29,18 +29,19 @@ pub struct SortedContainer {
 // }
 
 impl Data {
-
     pub fn new(age: i32, name: String) -> Self {
-        return Data { age:age, name:name };
+        return Data {
+            age: age,
+            name: name,
+        };
     }
-    
+
     pub fn compare(&self, ref d2: &Data) -> Ordering {
-        if self.age != d2.age { 
+        if self.age != d2.age {
             return self.age.cmp(&d2.age);
         }
         return self.name.cmp(&d2.name);
     }
-    
 }
 
 impl fmt::Display for Data {
@@ -52,97 +53,201 @@ impl fmt::Display for Data {
 
 impl Node {
     pub fn new(data: Data) -> Self {
-        return Node { data:data, left:None, right: None };
+        return Node {
+            data: data,
+            left: None,
+            right: None,
+        };
     }
     pub fn insert(&mut self, data: Data) {
         match self.data.compare(&data) {
             Ordering::Equal => {
                 return;
-            },
-            Ordering::Greater => {
-                match self.left {
-                    None => {
-                        self.left = Some(Box::new(Node::new(data)));
-                    },
-                    Some(ref mut node) => {
-                        node.insert(data);
-                    }
-                }
-            },
-            Ordering::Less => {
-                match self.right {
-                    None => {
-                        self.right = Some(Box::new(Node::new(data)));
-                    },
-                    Some(ref mut node) => {
-                        node.insert(data);
-                    }
-                }
             }
+            Ordering::Greater => match self.left {
+                None => {
+                    self.left = Some(Box::new(Node::new(data)));
+                }
+                Some(ref mut node) => {
+                    node.insert(data);
+                }
+            },
+            Ordering::Less => match self.right {
+                None => {
+                    self.right = Some(Box::new(Node::new(data)));
+                }
+                Some(ref mut node) => {
+                    node.insert(data);
+                }
+            },
         }
     }
     pub fn contains(&self, ref data: &Data) -> bool {
         match self.data.compare(&data) {
             Ordering::Equal => {
                 return true;
-            },
-            Ordering::Greater => {
-                match self.left {
-                    None => {
-                        return false;
-                    },
-                    Some(ref node) => {
-                        return node.contains(&data);
-                    }
-                }
-            },
-            Ordering::Less => {
-                match self.right {
-                    None => {
-                        return false;
-                    },
-                    Some(ref node) => {
-                        return node.contains(&data);
-                    }
-                }
             }
+            Ordering::Greater => match self.left {
+                None => {
+                    return false;
+                }
+                Some(ref node) => {
+                    return node.contains(&data);
+                }
+            },
+            Ordering::Less => match self.right {
+                None => {
+                    return false;
+                }
+                Some(ref node) => {
+                    return node.contains(&data);
+                }
+            },
         }
     }
 
     pub fn erase(&mut self, ref data: &Data) {
-
+        match self.data.compare(&data) {
+            Ordering::Equal => {
+                println!("Can't be happening.");
+            }
+            Ordering::Greater => match self.left.take() {
+                None => {
+                    return;
+                }
+                Some(mut node) => {
+                    match node.data.compare(&data) {
+                        // Node found, erase this one
+                        Ordering::Equal => match (node.left.take(), node.right.take()) {
+                            (None, None) => {}
+                            (Some(l), None) => self.left = Some(l),
+                            (None, Some(r)) => self.left = Some(r),
+                            (Some(l), Some(mut r)) => match r.left.take() {
+                                None => {
+                                    r.left = Some(l);
+                                    self.left = Some(r);
+                                }
+                                Some(mut lmc) => while true {
+                                    match lmc.left.take() {
+                                        None => break,
+                                        Some(mut lmcleft) => match lmcleft.left.take() {
+                                            None => {
+                                                match lmcleft.right.take() {
+                                                    None => {}
+                                                    Some(lmcleftright) => {
+                                                        lmc.left = Some(lmcleftright);
+                                                    }
+                                                }
+                                                lmcleft.left = Some(l);
+                                                lmcleft.right = Some(r);
+                                                self.left = Some(lmcleft);
+                                                break;
+                                            }
+                                            Some(superleft) => {
+                                                lmcleft.left = Some(superleft);
+                                                lmc.left = Some(lmcleft);
+                                            }
+                                        },
+                                    }
+                                },
+                            },
+                        },
+                        // Node not found, go recursively
+                        Ordering::Greater => {
+                            node.erase(&data);
+                            self.left = Some(node);
+                        }
+                        Ordering::Less => {
+                            node.erase(&data);
+                            self.left = Some(node);
+                        }
+                    }
+                }
+            },
+            Ordering::Less => match self.right.take() {
+                None => {
+                    return;
+                }
+                Some(mut node) => {
+                    match node.data.compare(&data) {
+                        // Node found, erase this one
+                        Ordering::Equal => match (node.left.take(), node.right.take()) {
+                            (None, None) => {}
+                            (Some(l), None) => self.right = Some(l),
+                            (None, Some(r)) => self.right = Some(r),
+                            (Some(l), Some(mut r)) => match r.left.take() {
+                                None => {
+                                    r.left = Some(l);
+                                    self.right = Some(r);
+                                }
+                                Some(mut lmc) => while true {
+                                    match lmc.left.take() {
+                                        None => break,
+                                        Some(mut lmcleft) => match lmcleft.left.take() {
+                                            None => {
+                                                match lmcleft.right.take() {
+                                                    None => {}
+                                                    Some(lmcleftright) => {
+                                                        lmc.left = Some(lmcleftright);
+                                                    }
+                                                }
+                                                lmcleft.left = Some(l);
+                                                lmcleft.right = Some(r);
+                                                self.right = Some(lmcleft);
+                                                break;
+                                            }
+                                            Some(superleft) => {
+                                                lmcleft.left = Some(superleft);
+                                                lmc.left = Some(lmcleft);
+                                            }
+                                        },
+                                    }
+                                },
+                            },
+                        },
+                        // Node not found, go recursively
+                        Ordering::Greater => {
+                            node.erase(&data);
+                            self.right = Some(node);
+                        }
+                        Ordering::Less => {
+                            node.erase(&data);
+                            self.right = Some(node);
+                        }
+                    }
+                }
+            },
+        }
     }
 
-	pub fn print_children(&self, indent: String) {
-		match self.left {
-			Some(ref node) => {
-				println!("{}{}", indent, node.data);
-				node.print_children(format!("{} ", indent));
-			}
-			None => {}
-		}
-		match self.right {
-			Some(ref node) => {
-				println!("{}{}", indent, node.data);
-				node.print_children(format!("{} ", indent));
-			}
-			None => {}
-		}
+    pub fn print_children(&self, indent: String) {
+        match self.left {
+            Some(ref node) => {
+                println!("{}{}", indent, node.data);
+                node.print_children(format!("{} ", indent));
+            }
+            None => {}
+        }
+        match self.right {
+            Some(ref node) => {
+                println!("{}{}", indent, node.data);
+                node.print_children(format!("{} ", indent));
+            }
+            None => {}
+        }
     }
 }
 
 impl SortedContainer {
-
-
     pub fn new() -> Self {
-        return SortedContainer{ root: None };
+        return SortedContainer { root: None };
     }
 
     pub fn insert(&mut self, data: Data) {
         match self.root {
             None => {
                 self.root = Some(Box::new(Node::new(data)));
-            },
+            }
             Some(ref mut node) => {
                 node.insert(data);
             }
@@ -153,7 +258,7 @@ impl SortedContainer {
         match self.root {
             None => {
                 return false;
-            },
+            }
             Some(ref root) => {
                 return root.contains(&data);
             }
@@ -173,50 +278,60 @@ impl SortedContainer {
     }
 
     pub fn erase(&mut self, ref data: &Data) {
-        match self.root {
+        let root_node = self.root.take();
+        match root_node {
             None => {
                 println!("Empty SortedContainer");
-                return
+                return;
             }
-            Some( mut root) => {
+            Some(mut root) => {
                 // create auxillary root
-                let mut aux : Node = Node::new(Data::new(0, "auxillary root".to_string()));
-                self.root = Some(Box::new(aux));
+                let mut aux: Node = Node::new(Data::new(0, "auxillary root".to_string()));
                 match aux.data.compare(&root.data) {
                     Ordering::Equal => {
-                        // auxillary root already exists => create a new one.
-                        self.root = Some(Box::new(Node::new(Data::new(-1000, "auxillary root".to_string()))));
-                        aux.left = Some(root);
-                    },
+                        panic!("Auxillary root already exists. Please name your root differently.");
+                    }
                     Ordering::Greater => {
                         aux.left = Some(root);
-                    },
+                    }
                     Ordering::Less => {
                         aux.right = Some(root);
                     }
                 }
+                self.root = Some(Box::new(aux));
 
                 // delete data
-                aux.erase(&data);
-
-                // remove auxillary root
-                match (aux.left, aux.right) {
-                    (None, None) => {
-                        self.root = None;
+                match self.root.take() {
+                    None => {
+                        println!("Empty root.");
                     }
-                    (Some(l), None) => {
-                        self.root = Some(l);
-                    }
-                    (None, Some(r)) => {
-                        self.root = Some(r);
-                    }
-                    (Some(l), Some(r)) => {
-                        println!("Something went wrong: auxillary root has 2 children");
+                    Some(mut root) => {
+                        root.erase(&data);
+                        self.root = Some(root);
                     }
                 }
-                
+
+                // remove auxillary root
+                match self.root.take() {
+                    None => {
+                        println!("Empty root.");
+                    }
+                    Some(mut root) => match (root.left.take(), root.right.take()) {
+                        (None, None) => {
+                            self.root = None;
+                        }
+                        (Some(l), None) => {
+                            self.root = Some(l);
+                        }
+                        (None, Some(r)) => {
+                            self.root = Some(r);
+                        }
+                        (Some(l), Some(r)) => {
+                            println!("Something went wrong: auxillary root has 2 children");
+                        }
+                    },
+                }
             }
         }
     }
-
 }
